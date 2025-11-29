e80eb35c8d1f83c8d18411bc524562603fabbf77
Jesus Gutierrez - Xeuz27 <jesusg.1301.jg@gmail.com>
2025-11-29 19:04:22 -0400

    chore: estilos? quitar cjs del bundle

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,8 +1,30 @@
 import { defineConfig } from "tsup"
 
+// const injectFunc = `
+// function injectStyle(css) {
+//   if (!css || typeof document === 'undefined') return
+
+//   const head = document.head || document.getElementsByTagName('head')[0]
+//   const style = document.createElement('style')
+//   style.type = 'text/css'
+
+//   if(head.firstChild) {
+//     head.insertBefore(style, head.firstChild)
+//   } else {
+//     head.appendChild(style)
+//   }
+
+//   if(style.styleSheet) {
+//     style.styleSheet.cssText = css
+//   } else {
+//     style.appendChild(document.createTextNode(css))
+//   }
+// }
+// `
+
 export default defineConfig({
 	entry: ["src/index.ts"], // o 'src/index.tsx' si usas JSX
-	format: ["cjs", "esm"], // outputs: dist/index.cjs / dist/index.mjs
+	format: ["esm"], // outputs: dist/index.cjs / dist/index.mjs
 	target: "esnext", // o "es2020" según necesites
 	sourcemap: true, // opcional, útil para debug
 	clean: true, // limpia la carpeta dist antes de compilar
@@ -11,7 +33,10 @@ export default defineConfig({
 	treeshake: true, //opcional, para eliminar codigo no usado
 	external: ["react", "react-dom"], // IMPORTANT: marcar peer deps como external
 	loader: {
-		".css": "file",
+		".css": "copy",
 	},
 	bundle: true, // agrupa todo (útil si importas css)
+	// injectStyle: (css) => {
+	// 	return `${injectFunc}injectStyle(${css});`
+	// },
 })
