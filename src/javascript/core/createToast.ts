4e266e3e17d18785d92017d9128844af7a9270e1
Jesus Gutierrez - Xeuz27 <yidev27@gmail.com>
2025-12-17 18:12:26 -0400

    feat/todo: display different icons on toast state

--- src/javascript/core/createToast.ts
+++ src/javascript/core/createToast.ts
@@ -1,10 +1,23 @@
 import type { Toast, ToastOptions } from "../../types/toast.js"
 
+import error from "../../assets/icons/error.svg"
 import { renderToast } from "../dom/renderToast.ts"
 import { addToastToState } from "../state.ts"
 
+const whatIcon = (type: string) => {
+	let icon: any = {
+		default: "",
+		loading: error,
+		success: "",
+		error: error,
+	}
+	//pasar 3 iconos en promesa, 1 solo en show?
+	console.log(icon[type], "sad", type)
+	return icon[type]
+}
 export function createToast(message: string, options: ToastOptions = {}) {
 	const id = crypto.randomUUID().slice(0, 6)
+	// console.log(whatIcon(options.type || "default"), "dsadasd")
 	const toast: Toast = {
 		id: id,
 		message: message,
@@ -13,7 +26,7 @@ export function createToast(message: string, options: ToastOptions = {}) {
 			type: options.type || "default",
 			duration: options.duration || 3000,
 			className: options.className || "",
-			icon: options.icon || "",
+			// icon: whatIcon(options.type || "default"),
 			styles: options.styles || {},
 		},
 		rendered: false,
